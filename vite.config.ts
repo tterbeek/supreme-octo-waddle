2ade07cc01cbd68622c32c2ad21f03b8e054bb73
ChiChiBeek <me@thijs.com>
2025-11-06 19:09:36 +0100

    New icons

--- vite.config.ts
+++ vite.config.ts
@@ -13,25 +13,34 @@ VitePWA({
     theme_color: "#fbbf24", // matches mustard UI
     background_color: "#ffffff",
     display: "standalone",
-    icons: [
-          {
-            src: "/icon.svg",
-            sizes: "512x512",
-            type: "image/svg+xml",
-            purpose: "any maskable"
-          },
-          {
-            src: "/pwa-192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "/pwa-512.png",
-            sizes: "512x512",
-            type: "image/png"
-          }
-        ]
-  }
+      "icons": [
+        {
+          "src": "public/icon-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+        {
+          "src": "public/icon-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png"
+        },
+        {
+          "src": "public/icon-180x180.png",
+          "sizes": "180x180",
+          "type": "image/png"
+        },
+        {
+          "src": "public/icon-256x256.png",
+          "sizes": "256x256",
+          "type": "image/png"
+        },
+        {
+          "src": "public/icon-150x150.png",
+          "sizes": "150x150",
+          "type": "image/png"
+        }
+      ]
+      }
 })
 ]
 });
